1fa4f3bbfb5cc383213c50d38a068c25faf1b2cb
Sasmith Manawadu <sasmithgit@gmail.com>
2024-08-09 20:26:00 +0530

    X-CRSF Optimized

---
+++ SpringSec/src/main/java/com/sasax/SpringSec/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sasax.SpringSec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
